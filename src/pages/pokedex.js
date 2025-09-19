ed1b7dd091025f5493eb218181a2e635164f9dfa
sashasohrabi <shb.sohrabi@gmail.com>
2025-09-19 11:31:58 +0200

    feat: implement Pokédex page list

--- src/pages/pokedex.js
+++ src/pages/pokedex.js
@@ -1,1 +1,15 @@
-console.log('pokedex.js');
+import { getFromLocalStorage, FAVORITES_KEY, renderUI } from '../lib';
+
+import PokemonCard from '../components/PokemonCard.js';
+
+const cardOptions = { catchBtn: false, favoriteBtn: false };
+
+const favs = getFromLocalStorage(FAVORITES_KEY);
+
+const favCards = favs.map((p) => PokemonCard(p, { ...cardOptions }));
+
+if (favs.length) {
+  renderUI('#pokedex-container', favCards, 'set');
+} else {
+  renderUI('#pokedex-container', '<h1> There is no favorite pokemone! </h1>', 'replace');
+}
