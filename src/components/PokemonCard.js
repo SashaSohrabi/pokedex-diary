c5382aa4d94e18bcd742f3b8dda6585d8e1888d1
Alexander U. <alexander.ulrich@ymail.com>
2025-09-21 23:12:41 +0200

    add footer with names, fix stats: now display correct values (#23)

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -11,14 +11,14 @@ const PokemonCard = (
   <div class="bg-green-600 h-2.5 rounded-full dark:bg-green-500" style="width: ${p.stats[0].value / 2.2}%"></div>
     <div class="mb-1 text-base font-medium text-yellow-700 dark:text-yellow-700">Attack ${p.stats[1].value}</div>
   <div class="bg-yellow-600 h-2.5 rounded-full dark:bg-yellow-500" style="width: ${p.stats[1].value / 2.2}%"></div>
-      <div class="mb-1 text-base font-medium text-orange-700 dark:text-orange-700">Defense ${p.stats[3].value}</div>
-  <div class="bg-orange-600 h-2.5 rounded-full dark:bg-orange-500" style="width: ${p.stats[3].value / 2.2}%"></div>
-    <div class="mb-1 text-base font-medium text-cyan-700 dark:text-cyan-700">Sp. Atk ${p.stats[4].value}</div>
-  <div class="bg-cyan-600 h-2.5 rounded-full dark:bg-cyan-500" style="width: ${p.stats[4].value / 2.2}%"></div>
-        <div class="mb-1 text-base font-medium text-blue-700 dark:text-blue-700">Sp. Def ${p.stats[5].value}</div>
-  <div class="bg-blue-600 h-2.5 rounded-full dark:bg-blue-500" style="width: ${p.stats[5].value / 2.2}%"></div>
-    <div class="mb-1 text-base font-medium text-purple-700 dark:text-purple-700">Speed ${p.stats[4].value}</div>
-  <div class="bg-purple-600 h-2.5 rounded-full dark:bg-purple-500" style="width: ${p.stats[4].value / 2.2}%" ></div>
+      <div class="mb-1 text-base font-medium text-orange-700 dark:text-orange-700">Defense ${p.stats[2].value}</div>
+  <div class="bg-orange-600 h-2.5 rounded-full dark:bg-orange-500" style="width: ${p.stats[2].value / 2.2}%"></div>
+    <div class="mb-1 text-base font-medium text-cyan-700 dark:text-cyan-700">Sp. Atk ${p.stats[3].value}</div>
+  <div class="bg-cyan-600 h-2.5 rounded-full dark:bg-cyan-500" style="width: ${p.stats[3].value / 2.2}%"></div>
+        <div class="mb-1 text-base font-medium text-blue-700 dark:text-blue-700">Sp. Def ${p.stats[4].value}</div>
+  <div class="bg-blue-600 h-2.5 rounded-full dark:bg-blue-500" style="width: ${p.stats[4].value / 2.2}%"></div>
+    <div class="mb-1 text-base font-medium text-purple-700 dark:text-purple-700">Speed ${p.stats[5].value}</div>
+  <div class="bg-purple-600 h-2.5 rounded-full dark:bg-purple-500" style="width: ${p.stats[5].value / 2.2}%" ></div>
   <div class="h-4"></div>
 
     </div>
